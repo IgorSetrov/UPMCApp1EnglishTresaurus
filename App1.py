7951881ca497e42d883ec23e2afff1471fa07df3
IgorSetrov <43652984+IgorSetrov@users.noreply.github.com>
2020-08-03 16:48:30 +0300

    Add confirmation from user recommended match

--- App1.py
+++ App1.py
@@ -9,7 +9,14 @@ def translate(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+        yes_or_no = input("Did you mean %s instead? Enter Y if yes, or N if no.: "
+                          % get_close_matches(w, data.keys())[0])
+        if yes_or_no == "Y" or yes_or_no == "y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yes_or_no == "N" or yes_or_no == "n":
+            return "The word doesn`t exist. Please double check it."
+        else:
+            return "We didn`t understand your entry."
     else:
         return "The word doesn`t exist. Please double check it."
 
