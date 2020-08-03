4ef82167b13302aaa482376cee51b770295b3d5b
IgorSetrov <43652984+IgorSetrov@users.noreply.github.com>
2020-08-03 17:07:01 +0300

    Add acronym processing

--- App1.py
+++ App1.py
@@ -10,6 +10,8 @@ def translate(w):
         return data[w]
     elif w.title() in data:
         return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yes_or_no = input("Did you mean %s instead? Enter Y if yes, or N if no.: "
                           % get_close_matches(w, data.keys())[0])
